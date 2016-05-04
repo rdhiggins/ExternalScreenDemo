81a355bd90e4bff0bb468b9a4cb6807f4f8a53bd
Rodger Higgins <rdhiggins@gmail.com>
2016-05-04 14:30:58 -0500

    Documented class

--- ExternalScreenDemo/MainScreenViewController.swift
+++ ExternalScreenDemo/MainScreenViewController.swift
@@ -26,34 +26,48 @@
 import UIKit
 import WebKit
 
+
+/// View controller used as the main view controller for the application.
+/// It is displayed on the devices main screen only.  It uses a WKWebView as
+/// its main view, which it loads a web site into..
 class MainScreenViewController: UIViewController {
 
-    private var webView: WKWebView?
+    /// Returns the view as a WKWebView
+    var webView: WKWebView? {
+        return view as? WKWebView
+    }
+    
+    /// Property used to store a reference to the external window object
+    /// when active
     private var externalWindow: UIWindow?
     
     deinit {
+        // Cleanup notifications
         NSNotificationCenter.defaultCenter().removeObserver(self)
     }
     
+    /// Overrided loadView method that is used to use a WKWebView
     override func loadView() {
-        webView = WKWebView()
-        view = webView
+        view = WKWebView()
         
-        // Check to see if there is an external screen already connected
-        if UIScreen.screens().count > 1 {
-            setupExternalScreen(UIScreen.screens()[1])
-        }
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        // Setup to process external screen notifications
         setupScreenNotifications()
         
+        // Load the WKWebView with something to show
         if let url = NSURL(string: "http://www.spazstik-software.com") {
             let req = NSURLRequest(URL: url)
             
             webView?.loadRequest(req)
+        }
+        
+        // Check to see if there is an external screen already connected
+        if UIScreen.screens().count > 1 {
+            setupExternalScreen(UIScreen.screens()[1])
         }
     }
 
@@ -62,6 +76,11 @@ class MainScreenViewController: UIViewController {
     }
     
 
+    /// A private method used to setup a external screen with a window
+    /// loaded with a ExternalScreenViewController
+    ///
+    /// - parameter screen: A UIScreen object to connect the 
+    /// ExternalScreenViewController too
     private func setupExternalScreen(screen: UIScreen) {
         guard externalWindow == nil,
             let vc = self.storyboard?.instantiateViewControllerWithIdentifier("ExternalScreen") as? ExternalScreenViewController else {
@@ -74,6 +93,8 @@ class MainScreenViewController: UIViewController {
         externalWindow!.hidden = false
     }
     
+    /// A private method used to teardown the external UIWindow object
+    /// used to display the content on the external screen
     private func teardownExternalScreen() {
         guard let ew = externalWindow else { return }
         
@@ -81,6 +102,10 @@ class MainScreenViewController: UIViewController {
         externalWindow = nil
     }
     
+    /// The method used to handle the external screen connection NSNoticiations.
+    ///
+    /// - parameter notification: A NSNotification object that informs us about
+    /// the UIScreen instance that has just become active
     func externalScreenDidConnect(notification: NSNotification) {
         guard let screen = notification.object as? UIScreen else {
             return
@@ -89,6 +114,11 @@ class MainScreenViewController: UIViewController {
         setupExternalScreen(screen)
     }
     
+    /// The method used to handle the notification for when a external screen
+    /// disconnect.
+    ///
+    /// - parameter notification: A NSNotification object that informs us about
+    /// the UIScreen instance that has just disconnected
     func externalScreenDidDisconnect(notification: NSNotification) {
         guard let _ = notification.object as? UIScreen else {
             return
@@ -97,6 +127,8 @@ class MainScreenViewController: UIViewController {
         teardownExternalScreen()
     }
     
+    /// Method used to register the notification handlers for external
+    /// screen connects/disconnects
     private func setupScreenNotifications() {
         let center = NSNotificationCenter.defaultCenter()
 
